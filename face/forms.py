a1b0c12417456b1f2e3a3f190da20a17bb36bf78
alex-klim <goldencrane@ukr.net>
2017-11-25 18:37:31 +0200

    change url field in the index form

--- face/forms.py
+++ face/forms.py
@@ -3,4 +3,4 @@ from django import forms
 
 class CrawlerForm(forms.Form):
     crawler = forms.CharField(label="crawler", max_length=20)
-    url = forms.CharField(label="url", max_length=100)
+    url = forms.URLField(label="url", max_length=100)
